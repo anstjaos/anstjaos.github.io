1092d75188722b47a88b5d220c41ad75d0bb56e2
anstjaos <vv_eros@naver.com>
2020-01-15 14:24:10 +0900

    _config.yml의 defaults _posts read_time false 변경

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"댓글이 잘 작동되나??  ","categories": ["Test"],
         "tags": ["Test"],
         "url": "http://localhost:4000/test/comments-test/",
+        "teaser":null},{
+        "title": "NHN 신입교육 1일차 후기",
+        "excerpt":"NHN 신입교육 후기 입사일인 2020년 1월 2일부터 7일간 회사 교육을 진행하였다. 서비스 교육 NHN이라는 회사가 현재 제공하고 있는 대표 서비스들에 대한 교육을 받았다. 서비스 팀의 이사님이나 수석님들께서 직접 오셔서 강연을 해주셔서 더욱 감동을 받았다. 설명을 들으며 회사에 대한 애정이 솟아나는 것 같았다. 개발자 교육 7일간 다양한 교육을 받았는데 그 중...","categories": ["NHN"],
+        "tags": ["NHN","신입교육"],
+        "url": "http://localhost:4000/nhn/newbie-education/",
         "teaser":null}]
