f0c1ccb49e8553a9963fe30d91dc77cc5689c731
anstjaos <vv_eros@naver.com>
2020-01-15 14:08:45 +0900

    Disqus를 통한 댓글 추가

---
+++ _posts/2020-01-15-comments-test.md
@@ -0,0 +1,11 @@
+---
+title:  "댓글 테스트"
+excerpt: "comments test"
+
+categories:
+  - Test
+tags:
+  - Test
+---
+
+댓글이 잘 작동되나??
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"안녕하세요 이 글의 제목은 github.io 블로그 시작하기입니다.  ","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/first-post/",
+        "teaser":null},{
+        "title": "댓글 테스트",
+        "excerpt":"댓글이 잘 작동되나??  ","categories": ["Test"],
+        "tags": ["Test"],
+        "url": "http://localhost:4000/test/comments-test/",
         "teaser":null}]
